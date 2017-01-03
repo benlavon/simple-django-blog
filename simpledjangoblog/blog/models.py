dbf94fbb4944c5681c22cc0d4a7363a7e1b54693
bennist <benlavon@gmail.com>
2017-01-03 04:14:48 -0500

    Finished writing the datababase models

--- simpledjangoblog/blog/models.py
+++ simpledjangoblog/blog/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
 
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class User(models.Model):
+	username = models.CharField(max_length=70)
+
+	def __str__(self):
+		return self.username
+
+class Post(models.Model):
+    pub_date = models.DateTimeField()
+    headline = models.CharField(max_length=200)
+    content = models.TextField()
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+    	return self.headline
+
+    def was_published_recently(self):
+    	return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
